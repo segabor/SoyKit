33a2821c7f94508afd5e53a637c151411657b087
Gábor Sebestyén <segabor@gmail.com>
2018-11-28 04:05:48 +0100

    Implement '-' (minus) operator

--- Sources/Soy/Types/SoyValue+MinusOperator.swift
+++ Sources/Soy/Types/SoyValue+MinusOperator.swift
@@ -7,64 +7,53 @@
 
 extension SoyValue {
     public static func - (lhs: SoyValue, rhs: SoyValue) -> SoyValue {
-        
+
         switch lhs {
         case .null:
             switch rhs {
             case .null, .bool(_), .integer(_):
-                return .integer(lhs.coerceToInteger + rhs.coerceToInteger)
-            case .double(_):
-                return rhs
+                return .integer(lhs.coerceToInteger - rhs.coerceToInteger)
+            case .double(let d):
+                return  d.isNaN ? rhs : .double(-d)
             default:
-                return .string(lhs.coerceToString + rhs.coerceToString)
+                return .double(.nan)
             }
-            
+
         case .bool(_):
             let stringLhs = lhs.coerceToString
             switch rhs {
             case .null, .bool(_), .integer(_):
-                return .integer(lhs.coerceToInteger + rhs.coerceToInteger)
+                return .integer(lhs.coerceToInteger - rhs.coerceToInteger)
             case .double(let f):
                 return f.isNaN || f.isInfinite
                     ?.string(stringLhs + rhs.coerceToString)
-                    : .double(Double(lhs.coerceToInteger) + f)
+                    : .double(Double(lhs.coerceToInteger) - f)
             case .string(_), .array(_), .map(_):
-                return .string(stringLhs + rhs.coerceToString)
+                return .double(.nan)
             }
-            
+
         case .integer(let n):
             switch rhs {
             case .null, .bool, .integer:
-                return .integer(n + rhs.coerceToInteger)
+                return .integer(n - rhs.coerceToInteger)
             case .double(let f):
-                return .double(Double(n) + f)
+                return .double(Double(n) - f)
             case .string(_), .array(_), .map(_):
-                return .string(lhs.coerceToString + rhs.coerceToString)
+                return .double(.nan)
             }
-            
+
         case .double(let d):
             switch rhs {
             case .null, .bool, .integer:
-                return d.isNaN || d.isInfinite ? lhs : .double(d + Double(rhs.coerceToInteger))
+                return d.isNaN ? lhs : .double(d - Double(rhs.coerceToInteger))
             case .double(let f):
-                return .double(d + f)
+                return .double(d - f)
             case .string(_), .array(_), .map(_):
-                return .string(lhs.coerceToString + rhs.coerceToString)
+                return .double(.nan)
             }
-            
-        case .string(let ls):
-            return .string(ls + rhs.coerceToString)
-            
-        case .array(let array):
-            switch rhs {
-            case .null, .bool(_), .integer(_), .double(_), .string(_), .map(_):
-                return .string(lhs.coerceToString + rhs.coerceToString)
-            case .array(let otherArray):
-                return .array(array + otherArray)
-            }
-            
-        case .map(_):
-            return .string(lhs.coerceToString + rhs.coerceToString)
+
+        case .string(_), .array(_), .map(_):
+            return .double(.nan)
         }
     }
 }
--- Tests/SoyTests/MinusOperatorTests.swift
+++ Tests/SoyTests/MinusOperatorTests.swift
@@ -13,78 +13,78 @@ class MinusOperatorTests: XCTestCase {
         let leftValue: SoyValue = nil
 
         XCTAssertEqual( leftValue - .null, SoyValue.integer(0) )
-        XCTAssertEqual( leftValue - .bool(true), SoyValue.integer(1) )
+        XCTAssertEqual( leftValue - .bool(true), SoyValue.integer(-1) )
         XCTAssertEqual( leftValue - .bool(false), SoyValue.integer(0) )
 
-        XCTAssertEqual( leftValue - .integer(123), SoyValue.integer(123) )
-        XCTAssertEqual( leftValue - .double(3.14), SoyValue.double(3.14) )
+        XCTAssertEqual( leftValue - .integer(123), SoyValue.integer(-123) )
+        XCTAssertEqual( leftValue - .double(3.14), SoyValue.double(-3.14) )
         if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
-        XCTAssertEqual( leftValue - .double(Double.infinity), .double(.infinity) )
+        XCTAssertEqual( leftValue - .double(Double.infinity), .double(-.infinity) )
         
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("nullalma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("null[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromBoolValue() {
         var leftValue: SoyValue = true
 
         XCTAssertEqual( SoyValue.integer(leftValue.coerceToInteger - SoyValue.null.coerceToInteger), SoyValue.integer(1) )
-        XCTAssertEqual( leftValue - .bool(true), .integer(2) )
+        XCTAssertEqual( leftValue - .bool(true), .integer(0) )
         XCTAssertEqual( leftValue - .bool(false), .integer(1) )
-        
-        XCTAssertEqual( leftValue - .integer(123), .integer(124) )
-        XCTAssertEqual( leftValue - .double(3.14), .double(4.140000000000001) )
+
+        XCTAssertEqual( leftValue - .integer(123), .integer(-122) )
+        XCTAssertEqual( leftValue - .double(3.14), .double(-2.14) )
         XCTAssertEqual( leftValue - .double(Double.nan), .string("trueNaN") )
         XCTAssertEqual( leftValue - .double(Double.infinity), .string("trueInfinity") )
-        
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("truealma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("true[object Object]") )
+
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
 
         leftValue = false
 
         XCTAssertEqual( SoyValue.integer(leftValue.coerceToInteger - SoyValue.null.coerceToInteger), SoyValue.integer(0) )
-        XCTAssertEqual( leftValue - .bool(true), .integer(1) )
+        XCTAssertEqual( leftValue - .bool(true), .integer(-1) )
         XCTAssertEqual( leftValue - .bool(false), .integer(0) )
 
-        XCTAssertEqual( leftValue - .integer(123), .integer(123) )
-        XCTAssertEqual( leftValue - .double(3.14), .double(3.14) )
+        XCTAssertEqual( leftValue - .integer(123), .integer(-123) )
+        XCTAssertEqual( leftValue - .double(3.14), .double(-3.14) )
         XCTAssertEqual( leftValue - .double(Double.nan), .string("falseNaN") )
         XCTAssertEqual( leftValue - .double(Double.infinity), .string("falseInfinity") )
         
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("falsealma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("false[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromIntegerValue() {
         let leftValue: SoyValue = 123
 
         XCTAssertEqual( leftValue - .null, .integer(123) )
-        XCTAssertEqual( leftValue - .bool(true), .integer(124) )
+        XCTAssertEqual( leftValue - .bool(true), .integer(122) )
         XCTAssertEqual( leftValue - .bool(false), .integer(123) )
 
-        XCTAssertEqual( leftValue - .integer(123), .integer(246) )
-        XCTAssertEqual( leftValue - .double(3.14), .double(126.14) )
+        XCTAssertEqual( leftValue - .integer(123), .integer(0) )
+        XCTAssertEqual( leftValue - .double(3.14), .double(119.86) )
         if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
-        XCTAssertEqual( leftValue - .double(Double.infinity), .double(Double.infinity) )
+        XCTAssertEqual( leftValue - .double(Double.infinity), .double(-.infinity) )
 
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("123alma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("123[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromDoubleValue() {
         let leftValue: SoyValue = 3.14
         
         XCTAssertEqual( leftValue - .null, .double(3.14) )
-        XCTAssertEqual( leftValue - .bool(true), .double(4.140000000000001) )
+        XCTAssertEqual( leftValue - .bool(true), .double(2.14) )
         XCTAssertEqual( leftValue - .bool(false), .double(3.14) )
 
-        XCTAssertEqual( leftValue - .integer(123), .double(126.14) )
-        XCTAssertEqual( leftValue - .double(3.14), .double(6.28) )
+        XCTAssertEqual( leftValue - .integer(123), .double(-119.86) )
+        XCTAssertEqual( leftValue - .double(3.14), .double(0) )
         if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
-        XCTAssertEqual( leftValue - .double(Double.infinity), .double(Double.infinity) )
+        XCTAssertEqual( leftValue - .double(Double.infinity), .double(-.infinity) )
 
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("3.14alma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("3.14[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromNaNValue() {
@@ -99,13 +99,13 @@ class MinusOperatorTests: XCTestCase {
         if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
         if case .double(let value) = leftValue - .double(Double.infinity), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("NaNalma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("NaN[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromInfinityValue() {
-        let leftValue: SoyValue = .double(Double.infinity)
-        let expectedValue = leftValue
+        var leftValue: SoyValue = .double(.infinity)
+        var expectedValue = leftValue
 
         XCTAssertEqual( leftValue - .null, expectedValue )
         XCTAssertEqual( leftValue - .bool(true), expectedValue )
@@ -114,57 +114,72 @@ class MinusOperatorTests: XCTestCase {
         XCTAssertEqual( leftValue - .integer(123), expectedValue )
         XCTAssertEqual( leftValue - .double(3.14), expectedValue )
         if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
-        XCTAssertEqual( leftValue - .double(Double.infinity), expectedValue )
+        if case .double(let value) = leftValue - .double(Double.infinity), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("Infinityalma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("Infinity[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
+
+        leftValue = .double(-.infinity)
+        expectedValue = leftValue
+
+        XCTAssertEqual( leftValue - .null, expectedValue )
+        XCTAssertEqual( leftValue - .bool(true), expectedValue )
+        XCTAssertEqual( leftValue - .bool(false), expectedValue )
+
+        XCTAssertEqual( leftValue - .integer(123), expectedValue )
+        XCTAssertEqual( leftValue - .double(3.14), expectedValue )
+        if case .double(let value) = leftValue - .double(Double.nan), value.isNaN {} else { XCTFail() }
+        XCTAssertEqual( leftValue - .double(Double.infinity), .double(-.infinity) )
+
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromStringValue() {
         let leftValue: SoyValue = "alma"
 
-        XCTAssertEqual( leftValue - .null, .string("almanull") )
-        XCTAssertEqual( leftValue - .bool(true), .string("almatrue") )
-        XCTAssertEqual( leftValue - .bool(false), .string("almafalse") )
+        if case .double(let value) = leftValue - .null, value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(true), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(false), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - .integer(123), .string("alma123") )
-        XCTAssertEqual( leftValue - .double(3.14), .string("alma3.14") )
-        XCTAssertEqual( leftValue - .double(Double.nan), .string("almaNaN") )
-        XCTAssertEqual( leftValue - .double(Double.infinity), .string("almaInfinity") )
+        if case .double(let value) = leftValue - .integer(123), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(3.14), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.nan), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.infinity), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - ["alma", 1, 3.14], .string("almaalma,1,3.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("alma[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromArrayValue() {
         let leftValue: SoyValue = ["alma", 1, 3.14]
 
-        XCTAssertEqual( leftValue - .null, .string("alma,1,3.14null") )
-        XCTAssertEqual( leftValue - .bool(true), .string("alma,1,3.14true") )
-        XCTAssertEqual( leftValue - .bool(false), .string("alma,1,3.14false") )
+        if case .double(let value) = leftValue - .null, value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(true), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(false), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - .integer(123), .string("alma,1,3.14123") )
-        XCTAssertEqual( leftValue - .double(3.14), .string("alma,1,3.143.14") )
-        XCTAssertEqual( leftValue - .double(Double.nan), .string("alma,1,3.14NaN") )
-        XCTAssertEqual( leftValue - .double(Double.infinity), .string("alma,1,3.14Infinity") )
+        if case .double(let value) = leftValue - .integer(123), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(3.14), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.nan), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.infinity), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - ["korte", 2, 4.14], .array(["alma", 1 , 3.14 , "korte", 2, 4.14]) )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("alma,1,3.14[object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 
     func testSubstractFromMapValue() {
         let leftValue: SoyValue = ["alma": 1, "korte": 3.14, "cseresznye": true]
 
-        XCTAssertEqual( leftValue - .null, .string("[object Object]null") )
-        XCTAssertEqual( leftValue - .bool(true), .string("[object Object]true") )
-        XCTAssertEqual( leftValue - .bool(false), .string("[object Object]false") )
+        if case .double(let value) = leftValue - .null, value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(true), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .bool(false), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - .integer(123), .string("[object Object]123") )
-        XCTAssertEqual( leftValue - .double(3.14), .string("[object Object]3.14") )
-        XCTAssertEqual( leftValue - .double(Double.nan), .string("[object Object]NaN") )
-        XCTAssertEqual( leftValue - .double(Double.infinity), .string("[object Object]Infinity") )
+        if case .double(let value) = leftValue - .integer(123), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(3.14), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.nan), value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - .double(.infinity), value.isNaN {} else { XCTFail() }
 
-        XCTAssertEqual( leftValue - ["korte", 2, 4.14], .string("[object Object]korte,2,4.14") )
-        XCTAssertEqual( leftValue - ["string": "alma", "int": 123], .string("[object Object][object Object]") )
+        if case .double(let value) = leftValue - ["alma", 1, 3.14], value.isNaN {} else { XCTFail() }
+        if case .double(let value) = leftValue - ["string": "alma", "int": 123], value.isNaN {} else { XCTFail() }
     }
 }
