f1d95f94458cf65149a8e3253bd81a3257e53f5b
Gábor Sebestyén <segabor@gmail.com>
2019-03-25 06:13:19 +0100

    Runtime support for soy functions

---
+++ Sources/SoyKit/Runtime/Runtime+Math.swift
@@ -0,0 +1,30 @@
+//
+//  Runtime+Math.swift
+//  Soy
+//
+//  Created by Gábr Sebestyén on 209. 02. 25..
+//
+
+#if os(macOS)
+import Darwin
+#elseif os(Linux)
+import Glibc
+#endif
+
+
+//
+// Math functions support
+//
+public extension Runtime {
+
+    static func round(_ value: SoyValue) -> SoyValue {
+        switch value {
+        case .integer:
+            return value
+        case .double(let d):
+            return .double( Darwin.round(d) )
+        default:
+            return .double(.nan)
+        }
+    }
+}
---
+++ Sources/SoyKit/Runtime/Runtime.swift
@@ -0,0 +1,10 @@
+//
+//  Runtime.swift
+//  Soy
+//
+//  Created by Gábor Sebestyén on 209. 02. 25..
+//
+
+
+public struct Runtime {}
+
