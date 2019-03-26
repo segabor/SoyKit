1661c5411dbd91ea8b638b74eb9bc71fa9e37640
Gábor Sebestyén <segabor@gmail.com>
2019-03-26 21:50:32 +0100

    Bump Swift version to 5

--- Package.swift
+++ Package.swift
@@ -1,12 +1,9 @@
-// swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "SoyKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SoyKit",
             targets: ["SoyKit"]),
@@ -16,8 +13,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SoyKit",
             dependencies: []),
