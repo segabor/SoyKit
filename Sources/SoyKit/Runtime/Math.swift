075df02ab38248544229be9fbb7589b3e4afda85
Gábor Sebestyén <segabor@gmail.com>
2019-04-04 20:48:12 +0200

    Implement sqrt and randomInt functions

--- Sources/SoyKit/Runtime/Math.swift
+++ Sources/SoyKit/Runtime/Math.swift
@@ -79,4 +79,34 @@ public struct Math {
 
         return d1 > d2 ? left : right
     }
+
+    public static func sqrt(_ value: SoyValue) throws -> SoyValue {
+        switch value {
+        case .integer(let i):
+            let d = Double(i)
+            #if os(macOS)
+            return .integer( Int(Darwin.sqrt(d)) )
+            #elseif os(Linux)
+            return .integer( Int(Glibc.sqrt(d)) )
+            #endif
+        case .double(let d):
+            #if os(macOS)
+            return .double( Darwin.sqrt(d) )
+            #elseif os(Linux)
+            return .double( Glibc.sqrt(d) )
+            #endif
+        default:
+            throw RuntimeError.invalidInput
+        }
+    }
+
+    public static func randomInt(_ value: SoyValue) throws -> SoyValue {
+        guard let limit = value.coerceToNumber?.intValue else {
+            throw RuntimeError.invalidInput
+        }
+
+        let r = Int.random(in: 0..<limit)
+
+        return .integer(r)
+    }
 }
