33ada0a40be1d116438aa7cd72617e63f31398b8
Gábor Sebestyén <segabor@gmail.com>
2019-05-09 21:31:28 +0200

    Implement strSub function

--- Sources/SoyKit/Runtime/Strings.swift
+++ Sources/SoyKit/Runtime/Strings.swift
@@ -31,7 +31,7 @@ public enum Strings {
 
     // strIndexOf(str, subStr)
     // Returns the first occurrence of substr within str,
-    // or -1. Case-sensitive, 0-based index.
+    //   or -1. Case-sensitive, 0-based index.
     public static func strIndexOf(_ value: SoyValue, _ subStringValue: SoyValue) throws -> SoyValue {
         guard case let .string(str) = value, case let .string(subStr) = subStringValue else {
             throw RuntimeError.invalidInput
@@ -41,5 +41,31 @@ public enum Strings {
             return .integer( str.distance(from: str.startIndex, to: subRange.lowerBound) )
         }
         return .integer( -1 )
+    }
+
+    // strSub(str, start)
+    // Returns the substring of str beginning at index start.
+    //
+    // strSub(str, start, end)
+    // Returns the substring of str beginning at index start,
+    //   and ending at end - 1.
+    public static func strSub(_ value: SoyValue, _ startIndexValue: SoyValue, _ endIndexValue: SoyValue = .integer(-1)) throws -> SoyValue {
+        guard case let .string(str) = value,
+            case let .integer(si) = startIndexValue,
+            case let .integer(ei) = endIndexValue
+        else {
+            throw RuntimeError.invalidInput
+        }
+
+        guard si >= 0,
+            (ei == -1 || ei >= si),
+            let lower = str.index(str.startIndex, offsetBy: si, limitedBy: str.endIndex),
+            let upper = ei == -1 ? str.endIndex : str.index(str.startIndex, offsetBy: ei, limitedBy: str.endIndex)
+        else {
+            // return null if range is invalid
+            return .null
+        }
+
+        return .string( String( str[lower..<upper] ))
     }
 }
